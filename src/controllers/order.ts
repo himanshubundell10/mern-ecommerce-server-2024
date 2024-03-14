afc1b69784bc4b9ae6cf353b4efae52fefce4e7c
himanshubundell10 <himanshubundel10@gmail.com>
2024-03-14 13:46:54 +0530

    Fix Issue While Placing New Order

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -72,48 +72,48 @@ export const getSingleOrder = TryCatch(async (req, res, next) => {
 
 
 export const newOrder = TryCatch(
-    async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
-      const {
-        shippingInfo,
-        orderItems,
-        user,
-        subtotal,
-        tax,
-        shippingCharges,
-        discount,
-        total,
-      } = req.body;
-  
-      if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
-        return next(new ErrorHandler("Please Enter All Fields", 400));
-  
-      const order = await Order.create({
-        shippingInfo,
-        orderItems,
-        user,
-        subtotal,
-        tax,
-        shippingCharges,
-        discount,
-        total,
-      });
-  
-      await reduceStock(orderItems);
-  
-      invalidateCache({
-        product: true,
-        order: true,
-        admin: true,
-        userId: user,
-        productId: order.orderItems.map((i) => String(i.productId)),
-      });
-  
-      return res.status(201).json({
-        success: true,
-        message: "Order Placed Successfully",
-      });
-    }
-  );
+  async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
+    const {
+      shippingInfo,
+      orderItems,
+      user,
+      subtotal,
+      tax,
+      shippingCharges,
+      discount,
+      total,
+    } = req.body;
+
+    if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
+      return next(new ErrorHandler("Please Enter All Fields", 400));
+
+    const order = await Order.create({
+      shippingInfo,
+      orderItems,
+      user,
+      subtotal,
+      tax,
+      shippingCharges,
+      discount,
+      total,
+    });
+
+    await reduceStock(orderItems);
+
+    invalidateCache({
+      product: true,
+      order: true,
+      admin: true,
+      userId: user,
+      productId: order.orderItems.map((i) => String(i.productId)),
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Order Placed Successfully",
+    });
+  }
+);
 
 //put request
 export const processOrder = TryCatch(async (req, res, next) => {
